f17cd4a6c09ec83c4e4a01a741e6cda6ad698263
Rohit Goyal <rg.rohitgoyal01@gmail.com>
2020-08-08 16:14:18 +0530

    first commit

---
+++ ci-webapp/src/main/java/com/lti/servlet/ProductServlet.java
@@ -0,0 +1,33 @@
+package com.lti.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.lti.dao.ProductDao;
+import com.lti.entity.Product;
+
+public class ProductServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		Product p = new Product();
+		p.setId(Integer.parseInt(request.getParameter("id")));
+		p.setName(request.getParameter("name"));
+		p.setPrice(Double.parseDouble(request.getParameter("price")));
+		
+		ProductDao dao = new ProductDao();
+		dao.insert(p);
+		
+		response.getWriter().write("product added successfully <a href ='addProduct.jsp'> Click Here </a> to add more ");
+		
+	}
+
+}
